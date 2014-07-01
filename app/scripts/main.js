8eab6c253c213e999d3b3c8da8b1f7ef5b08d05d
Tyler Berry <tyler.berry4@gmail.com>
2014-06-30 21:26:19 -0400

    Removed dist from gitignore, created Procfile and app.js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@ var flickrSecret = "b3ff002dc62fcf26" //secret
 //define variable for API key
 var flickrKey = "f1bfffb9390361590e7654a60cb2b6bf"; 
 
-$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickrKey + '&tags=bike%2C+trail%2c+bicycle&tag_mode=all&sort=interestingness-desc%2C+relevance&extras=url_z&per_page=3&format=json&nojsoncallback=1').done(function(bikeImgs) {
+$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickrKey + '&tags=bike%2C+trail&tag_mode=all&sort=interestingness-desc%2C+relevance&extras=url_z&per_page=3&format=json&nojsoncallback=1').done(function(bikeImgs) {
 	var markup = '';
 	var photos = bikeImgs.photos.photo;
 	_.each(photos, function(img) {
