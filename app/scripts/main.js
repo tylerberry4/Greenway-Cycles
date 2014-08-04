e85e71da0c0e1e25955d2555cefc33c07c8fe919
Tyler Berry <tyler.berry4@gmail.com>
2014-08-03 20:41:38 -0400

    changed nav color

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,26 +7,26 @@
 }).data('Swipe');*/
 
 //key
-var flickrSecret = "b3ff002dc62fcf26" //secret  
+// var flickrSecret = "b3ff002dc62fcf26" //secret  
 
-//define variable for API key
-var flickrKey = "f1bfffb9390361590e7654a60cb2b6bf"; 
+// //define variable for API key
+// var flickrKey = "f1bfffb9390361590e7654a60cb2b6bf"; 
 
-$.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickrKey + '&tags=bike%2C+trail&tag_mode=all&sort=interestingness-desc%2C+relevance&extras=url_z&per_page=3&format=json&nojsoncallback=1').done(function(bikeImgs) {
-	var markup = '';
-	var photos = bikeImgs.photos.photo;
-	_.each(photos, function(img) {
-		//populate the template
-		markup += '<img src="' + img.url_z + '">';
-    $('.photosContainer').html(markup);
-    //jQuery swipe: http://swipejs.com Copyright (c) 2013 Brad Birdsall Licensed under the The MIT License (MIT).
-    Swipe(document.getElementById('slider'), {
-            auto: 3000,
-            continuous: true,
-            speed: 400
-          });
-		$('.swipe-wrap').html(markup);
-	})
-});
+// $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=' + flickrKey + '&tags=bike%2C+trail&tag_mode=all&sort=interestingness-desc%2C+relevance&extras=url_z&per_page=3&format=json&nojsoncallback=1').done(function(bikeImgs) {
+// 	var markup = '';
+// 	var photos = bikeImgs.photos.photo;
+// 	_.each(photos, function(img) {
+// 		//populate the template
+// 		markup += '<img src="' + img.url_z + '">';
+//     $('.photosContainer').html(markup);
+//     //jQuery swipe: http://swipejs.com Copyright (c) 2013 Brad Birdsall Licensed under the The MIT License (MIT).
+//     Swipe(document.getElementById('slider'), {
+//             auto: 3000,
+//             continuous: true,
+//             speed: 400
+//           });
+// 		$('.swipe-wrap').html(markup);
+// 	})
+// });
     
 
